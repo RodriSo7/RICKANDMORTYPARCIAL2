96062bc87a08a9d565010a6600d165a5406c9664
ezete13 <telloezequiel8@gmail.com>
2023-06-23 17:19:27 -0300

    Buscador y Redireccionar al Home

--- src/app/services/servicio-personajes.service.ts
+++ src/app/services/servicio-personajes.service.ts
@@ -2,39 +2,41 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RootObject, infoRootObject } from '../interfaces/interfaces';
 
-
-
 @Injectable({
   providedIn: 'root'
 })
 export class ServicioPersonajesService {
 
   constructor(private http:HttpClient) { }
-  getMetodoPersonaje(){
+
+//HOME Personajes  
+getMetodoPersonaje(){
     return this.http.get<RootObject>("https://rickandmortyapi.com/api/character");
-  }
+}
 
-
-
-
-   /****Mostrar INFO Personajes****/
-   infoID: number=0;
-   asignarId(id: number) {
-    this.infoID = id;
-  }
-  
-  getDetallePersonaje(id: Number){
-    return this.http.get<infoRootObject>("https://rickandmortyapi.com/api/character/"+ id);      
-   }
+//DETALLES Personajes
+infoID: number=0;
    
-  /*FILTRO Personajes**/
-  getFiltroStatus(filtro: string) {     
+asignarId(id: number) {
+    this.infoID = id;
+}
+  
+getDetallePersonaje(id: Number){
+    return this.http.get<infoRootObject>("https://rickandmortyapi.com/api/character/"+ id);      
+}
+   
+//FILTRO Personajes
+getFiltroStatus(filtro: string) {     
     return this.http.get<RootObject>("https://rickandmortyapi.com/api/character/?status=" + filtro);
 }
 getFiltroSpecies(filtro: string) {     
     return this.http.get<RootObject>("https://rickandmortyapi.com/api/character/?species=" + filtro);
 }
-  
+
+//BUSCAR Personajes
+getBuscarPersonaje(texto: string) {
+    return this.http.get<RootObject>("https://rickandmortyapi.com/api/character/?name=" + texto);
+}
 
 
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ServicioPersonajesService } from '../services/servicio-personajes.service';
 import { Result } from '../interfaces/interfaces';
 import { Router } from '@angular/router';
-
+import { catchError } from 'rxjs/operators';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -11,41 +12,67 @@ import { Router } from '@angular/router';
 })
 export class Tab1Page implements OnInit{
 
+//Arreglo Personajes
 arregloInfo:Result[]=[];
 
-  constructor(private ServicioPersonajesService:ServicioPersonajesService, private router: Router) {}
+  constructor(private ServicioPersonajesService:ServicioPersonajesService, private router: Router, private alertController: AlertController) {}
+   
   ngOnInit() {
-    this.ServicioPersonajesService.getMetodoPersonaje().subscribe(respuesta=>{ console.log (respuesta);
-    this.arregloInfo=respuesta.results})
+      //HOME Personajes
+      this.ServicioPersonajesService.getMetodoPersonaje().subscribe(respuesta=>{ console.log (respuesta);
+      this.arregloInfo=respuesta.results})
     }
 
-
-    //Traer ID
-    idPersonaje(id: number){
+  // DETALLES Personajes
+  idPersonaje(id: number){// Esta funcion trae el id del HTML
       this.ServicioPersonajesService.asignarId(id);
+  }
+
+  //FILTRO Personajes
+  arrayFiltroItemStatus=["Alive", "Dead"];
+  arrayFiltroItemSpecies=["Human", "Alien"];
+
+  valorFiltroStatus(filtro: string) {
+      this.ServicioPersonajesService.getFiltroStatus(filtro).subscribe(respuestaFiltro => {
+      console.log(respuestaFiltro);
+      this.arregloInfo = respuestaFiltro.results;
+      })
+  }
+  valorFiltroSpecies(filtro: string) {
+      this.ServicioPersonajesService.getFiltroSpecies(filtro).subscribe(respuestaFiltro => {
+      console.log(respuestaFiltro);
+      this.arregloInfo = respuestaFiltro.results;
+      })  
+  }
+  salirFiltro() {
+      this.ngOnInit();
+  }
+
+  //BUSCADOR Personajes
+  texto: string = '';
+  async mostrarAlerta(titulo: string, mensaje: string) {
+    const alert = await this.alertController.create({
+      header: titulo,
+      message: mensaje,
+      buttons: ['Aceptar']
+    });
+    await alert.present();
+  }
+
+  enviarTexto(texto: string) {
+      this.ServicioPersonajesService.getBuscarPersonaje(texto)
+        
+        .pipe(
+          catchError(error => {
+            this.mostrarAlerta('Buscamos en el portal de Rick', 'Pero no encontramos resultados');
+            return [];
+          }))
+        
+        .subscribe(
+          respuestaBuscar => {
+            console.log(respuestaBuscar); 
+            this.arregloInfo = respuestaBuscar.results;
+          })
     }
-
-
-
-    /***FILTRO Personajes***/
-arrayFiltroItemStatus=["Alive", "Dead"];
-arrayFiltroItemSpecies=["Human", "Alien"];
-
-valorFiltroStatus(filtro: string) {
-  this.ServicioPersonajesService.getFiltroStatus(filtro).subscribe(respuestaFiltro => {
-    console.log(respuestaFiltro);
-    this.arregloInfo = respuestaFiltro.results;
-  })
-}
-valorFiltroSpecies(filtro: string) {
-  this.ServicioPersonajesService.getFiltroSpecies(filtro).subscribe(respuestaFiltro => {
-    console.log(respuestaFiltro);
-    this.arregloInfo = respuestaFiltro.results;
-  })  
-}
-salirFiltro() {
-  this.ngOnInit();
-}
-/**********/
 }
 
